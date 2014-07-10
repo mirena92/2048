5602a10f74f2577b09e847a4f531c229af8cebfc
mirena92 <mirena.k@abv.bg>
2014-07-10 14:52:27 +0300

    Add all movements

--- src/com/hackbulgaria/corejava/Game.java
+++ src/com/hackbulgaria/corejava/Game.java
@@ -23,33 +23,18 @@ public class Game {
         this.board = newBoard;
     }
     
-    public void moveDown() {
-        
+    public void moveDown() {    
         List<Integer> column = new ArrayList<>();
         List<Integer> newColumn = new ArrayList<>();
-        for(int y=0; y<boardDimensions; y++) {
-            
+        for(int y=0; y<boardDimensions; y++) {            
             column.clear();
             newColumn.clear();
             for(int x=boardDimensions-1; x>=0; x--) {
                 if(board[x][y]!=0) {
                     column.add(board[x][y]);
                 }
-            }
-            
-            if(!column.isEmpty()) {
-                for(int i=0; i<column.size(); i++) {
-                    if(i<column.size()-1 && column.get(i)==column.get(i+1)) {
-                        newColumn.add(column.get(i)*2);
-                        column.remove(i+1);
-                        column.add(0);                        
-                    }
-                    else {
-                        newColumn.add(column.get(i));
-                    }
-                }
-            }
-            
+            }            
+            createNewColumn(column, newColumn);   
             for(int x=boardDimensions-1; x>=0; x--) {
                 if (!newColumn.isEmpty()) {
                     board[x][y] = newColumn.get(0);
@@ -57,6 +42,105 @@ public class Game {
                 }
                 else {
                     board[x][y] = 0;
+                }
+            }
+        }
+    }
+    
+    public void moveUp() {      
+        List<Integer> column = new ArrayList<>();
+        List<Integer> newColumn = new ArrayList<>();
+        for(int y=0; y<boardDimensions; y++) {           
+            column.clear();
+            newColumn.clear();           
+            for(int x=0; x<boardDimensions; x++) {
+                if(board[x][y]!=0) {
+                    column.add(board[x][y]);
+                }
+            }
+            createNewColumn(column, newColumn);            
+            for(int x=0; x<boardDimensions; x++) {
+                if (!newColumn.isEmpty()) {
+                    board[x][y] = newColumn.get(0);
+                    newColumn.remove(0);
+                }
+                else {
+                    board[x][y] = 0;
+                }
+            }
+        }
+    }
+    
+    public void moveLeft() {      
+        List<Integer> column = new ArrayList<>();
+        List<Integer> newColumn = new ArrayList<>();
+        for(int x=0; x<boardDimensions; x++) {           
+            column.clear();
+            newColumn.clear();           
+            for(int y=0; y<boardDimensions; y++) {
+                if(board[x][y]!=0) {
+                    column.add(board[x][y]);
+                }
+            }
+            createNewColumn(column, newColumn);            
+            for(int y=0; y<boardDimensions; y++) {
+                if (!newColumn.isEmpty()) {
+                    board[x][y] = newColumn.get(0);
+                    newColumn.remove(0);
+                }
+                else {
+                    board[x][y] = 0;
+                }
+            }
+        }
+    }
+    
+    public void moveRight() {    
+        List<Integer> column = new ArrayList<>();
+        List<Integer> newColumn = new ArrayList<>();
+        for(int x=0; x<boardDimensions; x++) {            
+            column.clear();
+            newColumn.clear();
+            for(int y=boardDimensions-1; y>=0; y--) {
+                if(board[x][y]!=0) {
+                    column.add(board[x][y]);
+                }
+            }            
+            createNewColumn(column, newColumn);   
+            for(int y=boardDimensions-1; y>=0; y--) {
+                if (!newColumn.isEmpty()) {
+                    board[x][y] = newColumn.get(0);
+                    newColumn.remove(0);
+                }
+                else {
+                    board[x][y] = 0;
+                }
+            }
+        }
+    }
+    
+    public boolean isLost() {
+        
+        for(int i=0; i<boardDimensions; i++) {
+            for(int j=0; j<boardDimensions; j++) {
+                if(board[i][j]==0) {
+                    return false;                    
+                }
+            }
+        }
+        return true;
+    }
+    
+    private void createNewColumn(List<Integer> column, List<Integer> newColumn) {
+        if(!column.isEmpty()) {
+            for(int i=0; i<column.size(); i++) {
+                if(i<column.size()-1 && column.get(i)==column.get(i+1)) {
+                    newColumn.add(column.get(i)*2);
+                    column.remove(i+1);
+                    column.add(0);                        
+                }
+                else {
+                    newColumn.add(column.get(i));
                 }
             }
         }
@@ -81,5 +165,7 @@ public class Game {
             placeRandomNumber();
         }
     }
+    
+    
     
 }
--- src/com/hackbulgaria/corejava/MovementTestCases.java
+++ src/com/hackbulgaria/corejava/MovementTestCases.java
@@ -27,7 +27,7 @@ public class MovementTestCases {
     }
 
     @Test
-    public void test() {
+    public void testMoveDown() {
         int[][] expectedBoard = new int[][]{
                 {0,0,2,0},
                 {0,8,4,0},
@@ -35,8 +35,41 @@ public class MovementTestCases {
                 {0,4,8,0}
             };
         this.game.moveDown();
-
         assertArrayEquals(game.getBoard(), expectedBoard);
     }
+    
+    public void testMoveUp() {
+        int[][] expectedBoard = new int[][]{
+                {0,8,2,0},
+                {0,4,4,0},
+                {0,2,2,0},
+                {0,0,8,0}
+            };
+        this.game.moveUp();
+        assertArrayEquals(game.getBoard(), expectedBoard);
+    }
+    
+    public void testMoveLeft() {
+        int[][] expectedBoard = new int[][]{
+                {8,2,0,0},
+                {2,4,0,0},
+                {4,0,0,0},
+                {2,8,0,0}
+            };
+        this.game.moveLeft();
+        assertArrayEquals(game.getBoard(), expectedBoard);
+    }
+    
+    public void testMoveRight() {
+        int[][] expectedBoard = new int[][]{
+                {0,0,8,2},
+                {0,0,2,4},
+                {0,0,0,4},
+                {0,0,2,8}
+            };
+        this.game.moveRight();
+        assertArrayEquals(game.getBoard(), expectedBoard);
+    }
+
 
 }
