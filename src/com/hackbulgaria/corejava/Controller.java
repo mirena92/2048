1a6e68d690fa4ae3a2f420e5802becf12d6efcf1
mirena92 <mirena.k@abv.bg>
2014-07-14 23:08:49 +0300

    Key listener is ready

--- src/com/hackbulgaria/corejava/Controller.java
+++ src/com/hackbulgaria/corejava/Controller.java
@@ -54,7 +54,6 @@ public class Controller implements Serializable {
             this.game.moveRight();
         }
     }
-    
 
     public void load() {
         try {
--- src/com/hackbulgaria/corejava/GUI.java
+++ src/com/hackbulgaria/corejava/GUI.java
@@ -1,7 +1,12 @@
 package com.hackbulgaria.corejava;
 
+import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +14,7 @@ import java.util.Map;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class GUI extends JFrame implements Visualization {
 
@@ -27,7 +33,7 @@ public class GUI extends JFrame implements Visualization {
     private final int WIDTH = 400;
     private final int HEIGHT = 400;
 
-
+    
     private JLabel createLabel(int x, int y) {
         ImageIcon icon = new ImageIcon(mapImages.get(cntrl.game.getBoard()[x][y]));
         JLabel label = new JLabel(icon);
@@ -35,27 +41,17 @@ public class GUI extends JFrame implements Visualization {
     }
 
     @Override
-    public void displayBoard() throws IOException {
-       
+    public void displayBoard() throws IOException { 
+        
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationByPlatform(true);
         this.setVisible(true);
         this.setSize(WIDTH, HEIGHT);
         this.setLocationRelativeTo(null);
         this.setLayout(gridLayout);
-        KeyListener keyListener = new KeyListener();
-        this.addKeyListener(keyListener);
-       
+        
+        this.addKeyListener(new MyKeyListener());        
         printBoard();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        
-        getContentPane().removeAll();
-        
     }
 
     @Override
@@ -70,6 +66,43 @@ public class GUI extends JFrame implements Visualization {
         return false;
     }
 
+    private void printBoard() {
+        getContentPane().removeAll();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                add(createLabel(i, j));
+                System.out.print(cntrl.game.getBoard()[i][j] + "  ");
+            }
+            System.out.println();
+       }
+        System.out.println();
+    }
+    
+//    public class MyKeyListener implements KeyListener {
+        public class MyKeyListener extends KeyAdapter {        
+
+        public int keyType;
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+            System.out.println("In keyPressed");
+            if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                keyType = Keys.DOWN_ARROW.getNumber();
+            } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+                keyType = Keys.UP_ARROW.getNumber();
+            } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                keyType = Keys.LEFT_ARROW.getNumber();
+            } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                keyType = Keys.RIGHT_ARROW.getNumber();
+            } else if (e.getKeyCode() == KeyEvent.VK_N) {
+                keyType = Keys.N.getNumber();
+            }
+            cntrl.keyTypedGUI(keyType);
+            printBoard();     
+        }
+    }     
+    
     private static void initializeMap() throws IOException {
         String parent = "C:\\Users\\RUSHI\\Desktop\\coreJava\\2048\\images\\";
         mapImages.put(0, String.format("%s%s", parent, "empty.png"));
@@ -84,52 +117,5 @@ public class GUI extends JFrame implements Visualization {
         mapImages.put(512, String.format("%s%s", parent, "512.png"));
         mapImages.put(1024, String.format("%s%s", parent, "1024.png"));
         mapImages.put(2048, String.format("%s%s", parent, "2048.png"));
-    }
-
-    private void printBoard() {
-
-        getContentPane().removeAll();
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                add(createLabel(i, j));
-            }
-       }
-    }
-    
-    public class KeyListener implements java.awt.event.KeyListener {
-
-        public int keyType;
-        
-        @Override
-        public void keyTyped(KeyEvent e) {     
-        }
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-//            if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN) {
-//                keyType = Keys.DOWN_ARROW.getNumber();
-//            } else if (e.getExtendedKeyCode() == KeyEvent.VK_UP) {
-//                keyType = Keys.UP_ARROW.getNumber();
-//            } else if (e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
-//                keyType = Keys.LEFT_ARROW.getNumber();
-//            } else if (e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
-//                keyType = Keys.RIGHT_ARROW.getNumber();
-//            } else if (e.getExtendedKeyCode() == KeyEvent.VK_N) {
-//                keyType = Keys.N.getNumber();
-//            }
-//            cntrl.keyTypedGUI(keyType);
-//            printBoard();     
-            
-            if (e.getExtendedKeyCode() == KeyEvent.VK_N) {
-                printBoard();
-            }
-        }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-            if (e.getExtendedKeyCode() == KeyEvent.VK_N) {
-                printBoard();
-            }
-        }
     }
 }
