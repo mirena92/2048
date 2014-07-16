83d9697ed732c3694e035a75e887a443d8a724cb
mirena92 <mirena.k@abv.bg>
2014-07-16 23:40:47 +0300

    ..

--- src/com/hackbulgaria/corejava/Main.java
+++ src/com/hackbulgaria/corejava/Main.java
@@ -8,8 +8,7 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        SwingUtilities.invokeLater(new Runnable() {
-            
+        SwingUtilities.invokeLater(new Runnable() {           
             @Override
             public void run() {
                 Visualization gui = new GUI();
