d04f453c9bdbfe6a50f4268a153ad32d0a5c5b0f
mirena92 <mirena.k@abv.bg>
2014-07-17 11:54:12 +0300

    Add runnable jar

--- src/com/hackbulgaria/corejava/Main.java
+++ src/com/hackbulgaria/corejava/Main.java
@@ -8,7 +8,8 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        SwingUtilities.invokeLater(new Runnable() {           
+        SwingUtilities.invokeLater(new Runnable() {
+            
             @Override
             public void run() {
                 Visualization gui = new GUI();
